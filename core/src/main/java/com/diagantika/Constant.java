aadb8ec7857157a6c3e01d4f921396b8df924d6a
haslam <haslamhdz71@gmail.com>
2025-07-31 16:38:03 +0700

    update value virtual screen

--- core/src/main/java/com/diagantika/Constant.java
+++ core/src/main/java/com/diagantika/Constant.java
@@ -1,6 +1,6 @@
 package com.diagantika;
 
 public class Constant {
-    public static final int VIRTUAL_WIDTH = 960;
-    public static final int VIRTUAL_HEIGHT = 640;
+    public static final int VIRTUAL_WIDTH = 640;
+    public static final int VIRTUAL_HEIGHT = 360;
 }
