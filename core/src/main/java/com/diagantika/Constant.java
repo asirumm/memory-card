1dae30a955b4fcde0e4a75216c3c87481ff5ae22
haslam <haslamhdz71@gmail.com>
2025-08-04 13:19:07 +0700

    Update : screen target dan minimum

--- core/src/main/java/com/diagantika/Constant.java
+++ core/src/main/java/com/diagantika/Constant.java
@@ -1,6 +1,8 @@
 package com.diagantika;
 
 public class Constant {
-    public static final int VIRTUAL_WIDTH = 640;
-    public static final int VIRTUAL_HEIGHT = 360;
+    public static final int VIRTUAL_WIDTH = 320;
+    public static final int VIRTUAL_HEIGHT = 180;
+    public static int screenTargetWidth=1280;
+    public static int screenTargetHeight=720;
 }
