f53b4527cf475097d7e3f435b6e324b7c441ddde
haslam <haslamhdz71@gmail.com>
2025-08-04 17:14:27 +0700

    feat : load skin

--- core/src/main/java/com/diagantika/Main.java
+++ core/src/main/java/com/diagantika/Main.java
@@ -1,19 +1,42 @@
 package com.diagantika;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.diagantika.ScreenManager.ApplicationScreen;
 import com.diagantika.Transition.BidirectionalHorizontalTransition;
 import com.diagantika.Transition.HorizontalTransition;
 import com.diagantika.Transition.ScreenTransition;
+import org.tinylog.Logger;
 
 import java.util.HashMap;
 
+/**
+ * Remember the stageUI is already rendered at ApplicationScreen
+ * when you need another stage please render like
+ *
+ * stageUI.act();
+ * stageUI.getRoot().draw(batch,1);
+ *
+ * it cause we used framebuffer so beware using spritebatch
+ *
+ * and yah, im recommended to passing spritebatch instance from main
+ * when create a stage instance. Cause when you doesnt passing batch
+ * the stage will create another instance of spritebatch yet.
+ */
 public class Main extends ApplicationScreen {
 
     private HashMap<Transition,ScreenTransition> transition;
+    private Skin skin;
+    private BitmapFont bitmapFont;
 
     public enum Transition{
         HORIZONTAL_TRANSITION,
@@ -28,7 +51,8 @@ public class Main extends ApplicationScreen {
         transition.put(Transition.HORIZONTAL_TRANSITION,new HorizontalTransition());
         transition.put(Transition.BIDERECTIONAL_TRANSITION,new BidirectionalHorizontalTransition());
 
-//        setFirstScreen(new SplashScree());
+        loadSkin("Grand9KPixel.ttf","widget.atlas","widget.json");
+        setFirstScreen(new SplashScree());
     }
 
     public SpriteBatch getSpriteBath(){
@@ -45,7 +69,71 @@ public class Main extends ApplicationScreen {
         return super.frameBufferCamera;
     }
 
+    public Skin getSkin() {
+        return skin;
+    }
+
+    //TODO saat ini kita masih menggunakan 1 ukuran font kedepannya buat sesuai keinginan
+    public BitmapFont getBitmapFont() {
+        return bitmapFont;
+    }
+
     public OrthographicCamera screenCamera(){
         return super.screenCamera;
+    }
+
+    /**
+     * Note :
+     * - at your skin json dont include the bitmapfont or freetypefont
+     * delete that after you export from scene composer
+     * - make sure name of font is "font" if using another please change at
+     * this line code
+     *   skin.add("font",bitmapFont);
+     *
+     * ex:
+     * "com.badlogic.gdx.scenes.scene2d.ui.TextButton$TextButtonStyle": {
+     * 	"default": {
+     * 		"font": "font",
+     * 		"fontColor": "white",
+     * 		"up": "button"
+     * 	    }
+     * }
+     *
+     */
+    private void loadSkin(String fontName,
+                          String atlasFileName,
+                          String skinJsonFileName){
+
+        Logger.debug("start load skins and bitmap");
+
+        skin = new Skin();
+
+        FreeTypeFontGenerator freeTypeFontGenerator =
+                new FreeTypeFontGenerator(Gdx.files.internal(fontName));
+
+        FreeTypeFontGenerator.FreeTypeFontParameter parameters = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameters.size = 8;
+        parameters.magFilter = Texture.TextureFilter.Nearest;
+        parameters.minFilter = Texture.TextureFilter.Nearest;
+        parameters.genMipMaps = true;
+        parameters.color = Color.WHITE;
+        parameters.characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        bitmapFont = freeTypeFontGenerator.generateFont(parameters);
+
+        // dont forget dispose
+        freeTypeFontGenerator.dispose();
+
+        skin.add("font",bitmapFont);
+        skin.addRegions(new TextureAtlas(atlasFileName));
+        skin.load(Gdx.files.internal(skinJsonFileName));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bitmapFont.dispose();
+        skin.dispose();
     }
 }
