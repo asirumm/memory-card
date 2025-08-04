e2dfbfa0628859e2c3348748c41c8923e663394d
haslam <haslamhdz71@gmail.com>
2025-08-04 13:14:02 +0700

    Update : mendukung framebuffer

--- core/src/main/java/com/diagantika/Main.java
+++ core/src/main/java/com/diagantika/Main.java
@@ -1,10 +1,10 @@
 package com.diagantika;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.diagantika.ScreenManager.ApplicationScreen;
-import com.diagantika.ScreenManager.TransitionManager;
 import com.diagantika.Transition.BidirectionalHorizontalTransition;
 import com.diagantika.Transition.HorizontalTransition;
 import com.diagantika.Transition.ScreenTransition;
@@ -22,15 +22,13 @@ public class Main extends ApplicationScreen {
 
     @Override
     public void create() {
-        transitionManager = new TransitionManager();
-        viewport = new ExtendViewport(Constant.VIRTUAL_WIDTH,Constant.VIRTUAL_HEIGHT);
-        batch = new SpriteBatch();
+        super.create();
 
         transition = new HashMap<>();
         transition.put(Transition.HORIZONTAL_TRANSITION,new HorizontalTransition());
         transition.put(Transition.BIDERECTIONAL_TRANSITION,new BidirectionalHorizontalTransition());
 
-//        setFirstScreen(new SplashScreen());
+//        setFirstScreen(new SplashScree());
     }
 
     public SpriteBatch getSpriteBath(){
@@ -39,5 +37,15 @@ public class Main extends ApplicationScreen {
 
     public Viewport getViewport(){
         return viewport;
+    }
+    public Stage getStageUi(){
+        return super.stageUI;
+    }
+    public OrthographicCamera virtualCamera(){
+        return super.frameBufferCamera;
+    }
+
+    public OrthographicCamera screenCamera(){
+        return super.screenCamera;
     }
 }
